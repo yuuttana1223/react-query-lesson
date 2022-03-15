7b4d10c89030c7f51758a7ab72d58f9f71193240
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-15 10:36:31 +0900

    ✨ useContextを作成

---
+++ src/context/StateProvider.tsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+  VFC,
+} from "react";
+import { Task } from "types";
+
+type StateContextType = {
+  tasks?: Task[];
+  isDark: boolean;
+  setTasks: Dispatch<SetStateAction<Task[] | undefined>>;
+  setIsDark: Dispatch<SetStateAction<boolean>>;
+};
+
+const StateContext = createContext({} as StateContextType);
+
+type Props = {
+  children: ReactNode;
+};
+
+export const StateProvider: VFC<Props> = ({ children }) => {
+  const [tasks, setTasks] = useState<Task[] | undefined>(undefined);
+  const [isDark, setIsDark] = useState(false);
+
+  return (
+    <StateContext.Provider value={{ tasks, isDark, setTasks, setIsDark }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Task = {
+  id: number;
+  title: string;
+  tag: number;
+  tag_name: string;
+  created_at: string;
+  updated_at: string;
+};
